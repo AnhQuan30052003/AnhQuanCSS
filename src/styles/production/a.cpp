286349f7446d1dc0fd1f10a4460aa97a58afbd4a
Admin <anhquan300503@gmail.com>
2023-11-25 16:18:28 +0700

    25/11/2023: cập nhật thêm thuộc tính

--- src/styles/production/a.cpp
+++ src/styles/production/a.cpp
@@ -2,17 +2,32 @@
 #include <fstream>
 using namespace std;
 
+// void TaoClass();
+
 int main() {
   system("cls");
 
-  string fileName = "../file-module/button.css";
-  fstream file(fileName, ios::out);
+  string pathFile, fileName;
+  int soLanLap, giaTriBatDau;
+  string css, tenClass, thuocTinh, donVi, nhanManh;
 
-  int numberLoop = 100;
-  int value = 1;
-  for (short i = 1; i <= numberLoop; i++) {
-    file << ".fs-" << value << "{ font-size: " << value << "px; }" << endl;
-    value += 1;
+  fileName = "flex.css";
+  pathFile = "../file-module/" + fileName;
+  auto moFileDe = ios::app;
+
+  giaTriBatDau = 1;
+  soLanLap = 50;
+
+  tenClass = ".gap";
+  thuocTinh = "gap";
+  donVi = "px";
+  nhanManh = ""; // !important
+
+  fstream file(pathFile, moFileDe);
+  for (short i = 1; i <= soLanLap; i++) {
+    css = tenClass + "-" + to_string(i) + " { " + thuocTinh + ": " + to_string(i) + donVi + nhanManh + "; }";
+    file << css << endl;
+    giaTriBatDau += 1;
   }
 
   cout << "Build successfull.";
