d6888e42304fe98d0f1cc3f05c10c3469dcd21da
Admin <anhquan300503@gmail.com>
2024-01-06 17:05:31 +0700

    cập nhật

--- file/production/a.cpp
+++ file/production/a.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <iomanip>
+#include <sstream>
 #include <fstream>
 using namespace std;
 
@@ -8,29 +10,32 @@ int main() {
   string pathFile, fileName, css, tenClass, thuocTinh, donVi, nhanManh, am;
   int soLanLap, giaTriBatDau;
 
-  fileName = "padding.css";
+  fileName = "opacity.css";
   pathFile = "../module/" + fileName;
   auto moFileDe = ios::app;
 
   giaTriBatDau = 0;
   soLanLap = 100;
 
-  tenClass = "py";
-  thuocTinh = "padding-";
-  donVi = "px";
+  tenClass = "opacity";
+  thuocTinh = "opacity";
+  donVi = "";
 
   am = ""; // co or khong
   if (am.length() != 0) am = "-";
 
-  nhanManh = "co"; // co or khong
+  nhanManh = ""; // co or khong
   if (nhanManh.length() != 0) nhanManh = " !important";
 
   fstream file(pathFile, moFileDe);
-  for (short i = giaTriBatDau; i <= soLanLap; i++) {
-    css = "." + am + tenClass + "-" + to_string(i) + " { " + thuocTinh + ": " + am + to_string(i) + donVi + nhanManh + "; }";
+  for (int i = giaTriBatDau; i <= soLanLap; i++) {
+    stringstream stream;
+    stream << fixed << setprecision(2) << (float) i / 100;
+    string value = stream.str();
+    // css = "." + am + tenClass + "-" + to_string(i) + " { " + thuocTinh + ": " + am + to_string(i) + donVi + nhanManh + "; }";
     // css = "." + tenClass + "-" + to_string(i) + " { " + thuocTinh + "top: " + am + to_string(i) + donVi + nhanManh + "; " + thuocTinh + "bottom: " + am + to_string(i) + donVi + nhanManh + "; }";
+    // css = "." + am + tenClass + "-" + to_string(i) + " { " + thuocTinh + ": " + am + "" + value + "" + donVi + nhanManh + "; }";
     file << css << endl;
-    giaTriBatDau += 1;
   }
 
   cout << "Build successfull.";
