bb872f1f82fb71124b4ebc082f19e823ca901910
Admin <anhquan300503@gmail.com>
2024-01-08 10:42:16 +0700

    cập nhật

--- file/production/a.cpp
+++ file/production/a.cpp
@@ -7,33 +7,43 @@ using namespace std;
 int main() {
   system("cls");
 
-  string pathFile, fileName, css, tenClass, thuocTinh, donVi, nhanManh, am;
-  int soLanLap, giaTriBatDau;
+  string pathFile, fileName, css, tenClass, thuocTinh, donVi, nhanManh, am, dauTru, phanTram, tenClassCon1, tenClassCon2;
+  int giaTriKetThuc, giaTriBatDau;
 
-  fileName = "opacity.css";
+  fileName = "margin.css";
   pathFile = "../module/" + fileName;
   auto moFileDe = ios::app;
 
   giaTriBatDau = 0;
-  soLanLap = 100;
+  giaTriKetThuc = 500;
 
-  tenClass = "opacity";
-  thuocTinh = "opacity";
-  donVi = "";
+  tenClass = "my";
+  tenClassCon1 = "top";
+  tenClassCon2 = "bottom";
+  thuocTinh = "margin";
+  donVi = "px";
 
-  am = ""; // co or khong
-  if (am.length() != 0) am = "-";
+  phanTram = ""; // co or khong
+  if (phanTram.length() != 0) phanTram = "p";
 
-  nhanManh = ""; // co or khong
+  nhanManh = "co"; // co or khong
   if (nhanManh.length() != 0) nhanManh = " !important";
 
   fstream file(pathFile, moFileDe);
-  for (int i = giaTriBatDau; i <= soLanLap; i++) {
-    stringstream stream;
-    stream << fixed << setprecision(2) << (float) i / 100;
-    string value = stream.str();
-    // css = "." + am + tenClass + "-" + to_string(i) + " { " + thuocTinh + ": " + am + to_string(i) + donVi + nhanManh + "; }";
-    // css = "." + tenClass + "-" + to_string(i) + " { " + thuocTinh + "top: " + am + to_string(i) + donVi + nhanManh + "; " + thuocTinh + "bottom: " + am + to_string(i) + donVi + nhanManh + "; }";
+  for (int i = giaTriBatDau; i <= giaTriKetThuc; i++) {
+    am = "";
+    dauTru = "-";
+    if (i < 0) {
+      am = "-";
+      dauTru = "";
+    }
+    // css = "." + am + tenClass + dauTru + to_string(i) + phanTram + " { " + thuocTinh + ": " + to_string(i) + donVi + nhanManh + "; }";
+
+    css = "." + am + tenClass + dauTru + to_string(i) + phanTram + " { " + thuocTinh + dauTru + tenClassCon1 + ": " + am + to_string(i) + donVi + nhanManh + "; " + thuocTinh + dauTru + tenClassCon2 + ": " + am + to_string(i) + donVi + nhanManh + "; }";
+
+    // string value = stream.str();
+    // stringstream stream;
+    // stream << fixed << setprecision(2) << (float) i / 100;
     // css = "." + am + tenClass + "-" + to_string(i) + " { " + thuocTinh + ": " + am + "" + value + "" + donVi + nhanManh + "; }";
     file << css << endl;
   }
