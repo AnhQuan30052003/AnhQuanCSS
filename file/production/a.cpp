2a75d69044b964c1d67e779b50ff3b4ffa351042
Admin <anhquan300503@gmail.com>
2024-01-11 16:08:19 +0700

    cập nhật

--- file/production/a.cpp
+++ file/production/a.cpp
@@ -10,23 +10,23 @@ int main() {
   string pathFile, fileName, css, tenClass, thuocTinh, donVi, nhanManh, am, dauTru, phanTram, tenClassCon1, tenClassCon2;
   int giaTriKetThuc, giaTriBatDau;
 
-  fileName = "margin.css";
-  pathFile = "../module/" + fileName;
+  fileName = "min-width.css";
+  pathFile = "../modules/basic/" + fileName;
   auto moFileDe = ios::app;
 
   giaTriBatDau = 0;
-  giaTriKetThuc = 500;
+  giaTriKetThuc = 2000;
 
-  tenClass = "my";
-  tenClassCon1 = "top";
-  tenClassCon2 = "bottom";
-  thuocTinh = "margin";
+  tenClass = "min-w";
+  tenClassCon1 = "";
+  tenClassCon2 = "";
+  thuocTinh = "min-width";
   donVi = "px";
 
   phanTram = ""; // co or khong
   if (phanTram.length() != 0) phanTram = "p";
 
-  nhanManh = "co"; // co or khong
+  nhanManh = ""; // co or khong
   if (nhanManh.length() != 0) nhanManh = " !important";
 
   fstream file(pathFile, moFileDe);
@@ -37,9 +37,9 @@ int main() {
       am = "-";
       dauTru = "";
     }
-    // css = "." + am + tenClass + dauTru + to_string(i) + phanTram + " { " + thuocTinh + ": " + to_string(i) + donVi + nhanManh + "; }";
+    css = "." + am + tenClass + dauTru + to_string(i) + phanTram + " { " + thuocTinh + ": " + to_string(i) + donVi + nhanManh + "; }";
 
-    css = "." + am + tenClass + dauTru + to_string(i) + phanTram + " { " + thuocTinh + dauTru + tenClassCon1 + ": " + am + to_string(i) + donVi + nhanManh + "; " + thuocTinh + dauTru + tenClassCon2 + ": " + am + to_string(i) + donVi + nhanManh + "; }";
+    // css = "." + am + tenClass + dauTru + to_string(i) + phanTram + " { " + thuocTinh + dauTru + tenClassCon1 + ": " + am + to_string(i) + donVi + nhanManh + "; " + thuocTinh + dauTru + tenClassCon2 + ": " + am + to_string(i) + donVi + nhanManh + "; }";
 
     // string value = stream.str();
     // stringstream stream;
